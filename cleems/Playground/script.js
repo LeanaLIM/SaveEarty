3a1eb2ce539b8b7630e341892d1cde37dbd4a928
DJ-ust <121188328+DJ-ust@users.noreply.github.com>
2023-12-08 04:15:23 +0100

    last

--- cleems/Playground/script.js
+++ cleems/Playground/script.js
@@ -4,6 +4,8 @@ const contenu2 = document.getElementById('contenu2');
 const boutonToggle = document.getElementById('boutonToggle');
 const toggleImage = document.getElementById('toggleImage');
 const header = document.getElementById('header');
+const themeLink = document.getElementById('theme');
+
 
 // Fonction pour basculer entre les contenus
 function toggleContenu() {
@@ -11,15 +13,26 @@ function toggleContenu() {
         console.log("contenu 1 affiché")
         contenu1.style.display = 'none';
         contenu2.style.display = 'block';
-        header.style.background = 'blue';
-        toggleImage.src = '/cleems/Playground/images/terre.jpg';
+        // header.style.background = 'blue';
+        toggleImage.src = '/cleems/Playground/images/SVG_NOEL.svg';
+        setTheme('--background-color', 'blue'); // Couleur de fond lorsque la case est cochée
+        setTheme('--text-color', '#333'); // Couleur du texte lorsque la case est cochée
+
     } else {
         console.log("contenu 2 affiché")
-        contenu1.style.display = 'block';
+        contenu1.style.display = 'block';   
         contenu2.style.display = 'none';
-        header.style.background = 'green';
-        toggleImage.src = '/cleems/Playground/images/soleil.jpg';
+        // header.style.background = 'green';
+        toggleImage.src = '/cleems/Playground/images/SVG_.svg';
+        setTheme('--background-color', '#fff'); // Couleur de fond par défaut
+        setTheme('--text-color', '#333'); // Couleur du texte par défaut
     }
+
+    // Fonction pour définir une variable CSS
+function setTheme(variable, value) {
+    document.documentElement.style.setProperty(variable, value);
+}
+
 }
 
 // Ajouter un gestionnaire d'événements au bouton toggle
