c424450f889ed6ed2bb50c144b1fde3d291007e3
Dan PN <77532827+Derlvx@users.noreply.github.com>
2023-12-08 03:02:06 +0100

    estDansLeTableau

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
+import langues from "./langue/langue.json" assert { type: "json" };
+
+// Appel de l'API pour récupérer les langues disponibles
 const res = await fetch("https://libretranslate.org/languages", {
-	method: "GET",
-	headers: { "Content-Type": "application/json" }
+    method: "GET",
+    headers: { "Content-Type": "application/json" }
 });
 
+// récupération et attribution des langues
 let langDic = await res.json();
 
+// Ajout des langues dans le select
 langDic.forEach((lang) => {
     const option = document.createElement("option");
     option.value = lang.code;
     option.innerText = lang.name;
-    console.log(option);
-    document.querySelector("#addLanguages").appendChild(option);
+    document.querySelector("#Languages").appendChild(option);
 });
 
-// let alreadyAdded = ['French', 'English', 'Spanish'];
+// Tableau pour stocker les noms de langues déjà présent dans le JSON
+let nomsLangues = [];
 
+// Parcourir le JSON et ajouter les noms de langues au tableau
+langues.forEach(function (item) {
+    nomsLangues.push(item.Langue);
+});
 
-// var newTab = langDic.filter(function(objet) {
-//     return !alreadyAdded.includes(objet.name);
-// });
+// Filtre pour récupérer les langues qui ne sont pas dans le tableau
+let newTab = langDic.filter(function (objet) {
+    return !nomsLangues.includes(objet.name);
+});
 
-// console.log(newTab);
+console.log(newTab);
+
+$("#Languages").change(function () {
+    // Récupérer la valeur sélectionnée
+    var valeurSelectionnee = $(this).val();
+
+    var estDansLeTableau = newTab.some(function(objet) {
+        return objet.code === valeurSelectionnee;
+    });
+
+    if (estDansLeTableau) {
+        console.log("La valeur sélectionnée est dans le tableau d'objets.");
+        // Faire quelque chose si la valeur est dans le tableau
+    } else {
+        console.log("La valeur sélectionnée n'est pas dans le tableau d'objets.");
+        // Faire quelque chose si la valeur n'est pas dans le tableau
+    }
+
+});
 
 
 // const res = await fetch("https://libretranslate.org/translate", {
